d263b22ba2d44ba4d5d8039fe6d3b66512dc3a4a
Zaplatynskyy <zaplatynskyy@gmail.com>
2022-02-23 23:40:19 +0100

    chiamata axios + componenti

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,12 +1,40 @@
 <template>
     <div>
-        <h1>Funziona?</h1>
+        <Header/>
+        <Main/>
+        <Footer/>
     </div>
 </template>
 
 <script>
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+import Footer from './components/Footer.vue'
+
 export default {
-    name : 'App'
+    name : 'App',
+
+    components : {
+        Header,
+        Main, 
+        Footer
+    },
+
+    data() {
+        return {
+            posts : [] 
+        }
+    },
+
+    created() {
+        axios.get('http://localhost:8000/api/posts')
+
+        axios.get('http://localhost:8000/api/posts')
+            .then( response => {
+                this.posts = response.data;
+            })
+    }
+    
 }
 </script>
 
